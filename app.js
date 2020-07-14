a44b562737b4c12b4a6db12ac069adaeaf3f3382
Arjun <arjun2000raj@gmail.com>
2020-07-14 21:29:13 +0530

    .

--- app.js
+++ app.js
@@ -12,10 +12,5 @@ app.use(express.static(`${__dirname}/public`))
 
 //ROUTES
 app.use('/', viewRouter)
-app.get('/login', (req, res) => {
-    res.status(200).render('login', {
-        title: 'Login'
-    })
-})
 
 module.exports = app;
