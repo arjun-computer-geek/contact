a30e34bad7eb3f1c18dc32e6b1094f79cf148ae0
Arjun <arjun2000raj@gmail.com>
2020-07-15 01:48:30 +0530

    updated some features

--- public/js/script.js
+++ public/js/script.js
@@ -25,14 +25,3 @@ function searchContact() {
     }
   }
 
-// login form js
-
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
