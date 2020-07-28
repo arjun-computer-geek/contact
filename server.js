eefef32b2b4f5c5bf78ff9766bd541912d94351f
Arjun <arjun2000raj@gmail.com>
2020-07-28 12:44:21 +0530

    deploying

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(DB, {
 })
 
 
-const port = 8000
+const port = process.env.PORT || 8000
 const server = app.listen(port, () =>{
     console.log(`app running on port ${port}....`)
 })
